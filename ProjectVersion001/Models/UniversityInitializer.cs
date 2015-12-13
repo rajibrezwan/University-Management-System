d348cd19128be384403a8002bf7007c8e289ab6b
irreversible <irreversible>
2015-12-13 18:48:55 +0600

    University Management System .
    Simple and easy to use university management system . Add department ,
    add course , add teacher , add student . , assign teacher to course .
    Assign classroom . Enroll student to course . Add result . See results

---
+++ ProjectVersion001/Models/UniversityInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectVersion001.Models
+{
+    public class UniversityInitializer
+    {
+    }
+}
